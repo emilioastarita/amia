b341c7235e6ee1443faf311ed5de8b6bdf6f778d
chulo <emilio.astarita@gmail.com>
2015-03-12 09:20:29 -0300

    Orden de la configuración en las keys.

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 import nconf = require('nconf');
 nconf.argv()
      .env()
-     .add('default', { type: 'file', file: 'config-default.json'})
-     .add('local', { type: 'file', file: 'config-local.json' });
+     .add('local', { type: 'file', file: 'config-local.json' })
+     .add('default', { type: 'file', file: 'config-default.json'});
 
 export = nconf;
